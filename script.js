a34ebe7b03e679e88fd76ab460fb1e915108a68b
kyowaiiii <kyowaiabcdeh@gmail.com>
2026-02-22 23:01:54 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,80 +1,77 @@
-// TERMINAL ANIMATION
-
-const lines = [
-"root@senna:~$ initializing system",
-"loading database...",
-"checking security...",
-"loading scripts...",
-"system ready ✔"
-]
-
-let index = 0
-let terminal = document.getElementById("terminalText")
-
-function typeLine(){
-
-if(index < lines.length){
-
-let line = lines[index]
-let char = 0
-
-let typing = setInterval(()=>{
-
-terminal.innerHTML += line[char]
-
-char++
-
-if(char === line.length){
-
-clearInterval(typing)
-terminal.innerHTML += "\n"
-index++
-
-setTimeout(typeLine,500)
-
+function playClick(){
+let sound=document.getElementById("clickSound")
+sound.currentTime=0
+sound.play()
 }
 
-},40)
+function toggleCategory(){
 
+playClick()
+
+let list=document.getElementById("category-list")
+
+if(list.style.display==="block"){
+list.style.display="none"
+}else{
+list.style.display="block"
 }
 
 }
 
-typeLine()
+function downloadScript(){
 
+playClick()
 
-// SEARCH SCRIPT
+window.open(
+"https://example.com",
+"_blank"
+)
 
-const search = document.getElementById("search")
-const loader = document.getElementById("loader")
-const cards = document.querySelectorAll(".card")
+}
 
-search.addEventListener("keypress", function(e){
+function searchScript(){
 
-if(e.key === "Enter"){
+let input=document
+.getElementById("search")
+.value.toLowerCase()
 
-loader.style.display="block"
-
-setTimeout(()=>{
-
-loader.style.display="none"
-
-let value = search.value.toLowerCase()
+let cards=document
+.querySelectorAll(".script-card")
 
 cards.forEach(card=>{
 
-let name = card.innerText.toLowerCase()
+let text=card
+.innerText
+.toLowerCase()
 
-if(name.includes(value)){
-card.style.display="block"
-}else{
-card.style.display="none"
-}
-
-})
-
-},1500)
-
-}
+card.style.display=
+text.includes(input)
+? "block"
+: "none"
 
 })
+
+}
+
+const text="Initializing Senna Script..."
+
+let i=0
+
+function terminal(){
+
+let term=document
+.querySelector(".term-text")
+
+if(i<text.length){
+
+term.innerHTML+=text.charAt(i)
+
+i++
+
+setTimeout(terminal,50)
+
+}
+
+}
+
+terminal()
